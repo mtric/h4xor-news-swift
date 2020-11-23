c33eaeccd9748d43d046b510fb6e920d85854d51
mtric <29257165+mtric@users.noreply.github.com>
2020-11-23 13:36:14 +0100

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Eric Walter on 23.11.20.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
